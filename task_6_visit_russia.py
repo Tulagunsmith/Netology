e33be349c775e0b02010db7d295674971d69e6b0
Tulagunsmith <tulagunsmith@gmail.com>
2022-05-29 16:39:51 +0300

    only russian places of interest added

---
+++ task_6_visit_russia.py
@@ -0,0 +1,22 @@
+geo_logs = [
+    {'visit1': ['Москва', 'Россия']},
+    {'visit2': ['Дели', 'Индия']},
+    {'visit3': ['Владимир', 'Россия']},
+    {'visit4': ['Лиссабон', 'Португалия']},
+    {'visit5': ['Париж', 'Франция']},
+    {'visit6': ['Лиссабон', 'Португалия']},
+    {'visit7': ['Тула', 'Россия']},
+    {'visit8': ['Тула', 'Россия']},
+    {'visit9': ['Курск', 'Россия']},
+    {'visit10': ['Архангельск', 'Россия']}
+]
+geo_logs_filtered = []
+
+
+for logs in geo_logs:
+    for visit, place in list(logs.items()):
+        if 'Россия' in place:
+            geo_logs_filtered.append(logs)
+
+
+print(geo_logs_filtered)
